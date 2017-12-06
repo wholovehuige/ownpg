f04da319f4d6c83dc2250549378a78ed35f4970b
wholovehuige <673713797@qq.com>
2017-12-06 11:50:49 +0800

    a

--- mode/test1.py
+++ mode/test1.py
@@ -1,1 +1,23 @@
-print("hello world")
+import pymysql
+
+conn = pymysql.Connect(host="127.0.0.1"
+                       ,port=3306
+                       ,user="root"
+                       ,passwd="password"
+                       ,db="apple"
+                       ,charset="utf8")
+cursor = conn.cursor()
+def get():
+    sql = "select * from city limit 10"
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        print(row)
+
+def update():
+    sql = " "
+
+print(conn)
+print(cursor)
+
+cursor.close()
+conn.close()
