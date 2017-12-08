9a1ecc3bbc1ae919ad66b560e9534ffb0773415a
wholovehuige <673713797@qq.com>
2017-12-08 11:33:43 +0800

    ignore

--- mysite/blog1/urls.py
+++ mysite/blog1/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url,include
 from . import views
 urlpatterns = [
     url(r'^index/$',views.index),
-    url(r'^article/(?P<article_id>[0-9]+)$',views.article_page)
+    url(r'^article/save/$',views.article_save,name="article_save"),
+    url(r'^edit/(?P<article_id>[0-9]+)$',views.edit_page,name="edit_page"),
+    url(r'^edit/action/$',views.edit_action,name="edit_action"),
+    url(r'^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page')
 ]
--- mysite/blog1/views.py
+++ mysite/blog1/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from . import models
 # Create your views here.
 
@@ -9,3 +10,25 @@ def index(request):
 def article_page(request,article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request,'blog1/article_page.html',{'article':article})
+
+def edit_page(request,article_id):
+    if str(article_id) == '0':
+        return render(request,'blog1/edit_page.html')
+    article = models.Article.objects.get(pk = article_id)
+    return render(request,'blog1/edit_page_save.html',{"article":article})
+
+def edit_action(request):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    articles = models.Article.objects.all()
+    models.Article.objects.create(title=title,content=content)
+    return render(request,"blog1/index.html",{'articles':articles})
+
+def article_save(request):
+    id = request.POST.get("article_id")
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    t1 = models.Article(id = id,title = title,content=content)
+    t1.save()
+    return HttpResponse("<p>数据更新成功</>")
+
